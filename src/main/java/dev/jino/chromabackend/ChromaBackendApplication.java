3850cf69f1a5d4c0dfe8a49784892f97085d2db0
treasure-sky <ych0486666@gmail.com>
2024-12-02 13:25:19 +0900

    init: 프로젝트 생성

---
+++ src/main/java/dev/jino/chromabackend/ChromaBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.jino.chromabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChromaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChromaBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/jino/chromabackend/ChromaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jino.chromabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChromaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
